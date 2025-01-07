a07dc7b7cf6197cc1921091ec5692a03fae5ff33
mattmatt1234 <mattcastillo514@gmail.com>
2025-01-07 15:14:21 -0600

    Adding color theme constant

---
+++ lib/constants/colorThemeConstant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FitBuddyColorConstants {
+  static Color lPrimary = Colors.white;
+  static Color lOnPrimary = Colors.black;
+  static Color lSecondary = const Color(0xFFF2F2F2);
+  static Color lOnSecondary = const Color(0xFF757575);
+  static Color lAccent = const Color(0xFF4D54F0);
+  static Color lError = const Color(0xFFD32F2F);
+
+}
