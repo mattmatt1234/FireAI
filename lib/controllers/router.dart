ac7f6672cac6e52be369cbe0cfc67d64544ae4f6
mattmatt1234 <mattcastillo514@gmail.com>
2025-01-07 19:04:47 -0600

    Committing any other changes I may have done before switching to main branch

---
+++ lib/controllers/router.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fire_ai/constants/routeConstants.dart';
+
+import 'package:fire_ai/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../pages/accountCreation.dart';
+//import 'firestore/firestore_service.dart';
+//import 'notifier.dart';
